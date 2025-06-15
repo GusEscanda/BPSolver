bd0a0ab1bf2702247bfa9e904dfad11c82889b69
GusEscanda <gescandarani@chattigo.com>
2025-06-15 17:31:21 -0300

    Bot improvements

--- bot.py
+++ bot.py
@@ -15,6 +15,65 @@ from BPSolver import BPSolver
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 
+def try_solutions(image):
+    messages, result_image = ([], None)
+
+    # Find the puzzle's grid
+    grid = Grid(image=image)
+    grid.preprocess_image(
+        resize_width=500,
+        threshold_values=[70, 100, 130, 160]
+    )
+    grid.find_grid(
+        min_line_length=[300, 350, 400], 
+        max_line_gap=[8, 12], 
+        max_groupping_dist=3,
+        min_valid_n=5,
+        max_valid_n=15
+    )
+    if grid.n == 0:
+        messages.append("Could't find any board")
+        return (messages, result_image)
+    else:
+        result_image = draw_grid(grid.image, grid.x_axis, grid.y_axis)
+
+    # Try a Queens puzzle
+
+    puzzle = Queens(grid)
+    solver = BPSolver(puzzle, 1000, timedelta(seconds=120), 20)
+
+    if puzzle.msg != 'OK':
+        messages.append(f'Queens: {puzzle.msg}')
+    else:
+        msg = solver.solve()
+        if solver.solved:
+            messages = ["Queens board found, here's your solution!!"]
+            result_image = puzzle.draw_solution(grid.image)
+            return (messages, result_image)
+        messages.append(f'Queens: {msg}')
+
+    # Try a Tango puzzle
+
+    puzzle = Tango(
+        grid,
+        eq_filename='templates_eq.png',
+        x_filename='templates_x.png'
+    )
+    solver = BPSolver(puzzle, 1000, timedelta(seconds=120), 20)
+
+    if puzzle.msg != 'OK':
+        messages.append(f'Tango: {puzzle.msg}')
+    else:
+        msg = solver.solve()
+        if solver.solved:
+            messages = ["Tango board found, here's your solution!!"]
+            result_image = puzzle.draw_solution(grid.image)
+            return (messages, result_image)    
+        messages.append(f'Tango: {msg}')
+
+    return (messages, result_image)
+
+
 async def handle_image(update: Update, context: ContextTypes.DEFAULT_TYPE):
     photo = update.message.photo[-1]
     file = await context.bot.get_file(photo.file_id)
@@ -27,68 +86,16 @@ async def handle_image(update: Update, context: ContextTypes.DEFAULT_TYPE):
     img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
 
     try:
-        grid = Grid(image=img)
-        threshold_values = [70, 100, 130, 160]
-        min_line_length = [300, 350, 400]
-        max_line_gap = [8, 12]
-        max_groupping_dist = 3
-
-        solved = False
-        if not solved and grid.image is not None:
-            print('Trying to find a Queens puzzle')
-            await update.message.reply_text('Trying to find a Queens puzzle')
-            puzzle = Queens(
-                grid, 
-                resize_width=500, 
-                threshold_value= threshold_values,
-                min_line_length= min_line_length, 
-                max_line_gap= max_line_gap,
-                max_groupping_dist= max_groupping_dist,
-                min_valid_n= 6, max_valid_n= 16
-            )
-            print(puzzle.msg)
-            await update.message.reply_text(puzzle.msg)
-            solver = BPSolver(puzzle, 1000, timedelta(seconds=120), 20)
-            msg = solver.solve()
+        messages, result_image = try_solutions(img)
+        for msg in messages:
             print(msg)
             await update.message.reply_text(msg)
-            solved = solver.solved
-        if not solved and grid.image is not None:
-            print('Trying to find a Tango puzzle')
-            await update.message.reply_text('Trying to find a Tango puzzle')
-            puzzle = Tango(
-                grid, 
-                resize_width=500, 
-                threshold_value= threshold_values,
-                min_line_length= min_line_length, 
-                max_line_gap= max_line_gap,
-                max_groupping_dist= max_groupping_dist,
-                eq_filename= 'templates_eq.png',
-                x_filename='templates_x.png'
-            )
-            print(puzzle.msg)
-            await update.message.reply_text(puzzle.msg)
-            solver = BPSolver(puzzle, 1000, timedelta(seconds=120), 20)
-            msg = solver.solve()
-            print(msg)
-            await update.message.reply_text(msg)
-            solved = solver.solved
-
-        if grid.image is not None:
-            if solved:
-                result_img = puzzle.draw_solution(grid.image)
-            elif grid.n > 0:
-                result_img = draw_grid(grid.image, grid.x_axis, grid.y_axis)
-            else:
-                result_img = grid.image
-
-            # Send result with Telegram
-            _, buffer = cv2.imencode(".jpg", result_img)
+        if result_image is not None:
+            _, buffer = cv2.imencode(".jpg", result_image)
             output_io = BytesIO(buffer.tobytes())
             output_io.seek(0)
-
             await update.message.reply_photo(photo=output_io)
-
+        
     except Exception as e:
         await update.message.reply_text(f"Error processing the image: {e}")
 
